8346f0471468d7984bf57ec26d55378f6a81e830
Senthil Kuppuswamy <senthilkuppuswamy.rvs@gmail.com>
2022-01-15 11:47:35 +0530

    seventh

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ if(process.env.NODE_ENV === "production"){
 app.use(express.static('client/public'));
 //app.use('/src', express.static(path.join(__dirname, '..', 'client', 'src')))
 app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'client', 'public',index.html));
+    res.sendFile(path.resolve(__dirname,'client', 'public','index.html'));
 });
 }
 
