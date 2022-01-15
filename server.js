6e2f776837e9d1075f3753f0292703a61591c37b
Senthil Kuppuswamy <senthilkuppuswamy.rvs@gmail.com>
2022-01-15 12:24:42 +0530

    11th

--- server.js
+++ server.js
@@ -16,9 +16,8 @@ app.use(cors());
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, 'client')));
     app.use('/src', express.static(path.join(__dirname, '..', 'client', 'public')))
-    app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'client/public','index.html'));
-});
+    
+
 }
 
 
