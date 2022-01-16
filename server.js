25c8bc3d00a6b2a76ec11825bc0b31efd3a071f8
Senthil Kuppuswamy <senthilkuppuswamy.rvs@gmail.com>
2022-01-16 15:18:26 +0530

    8th

--- server.js
+++ server.js
@@ -13,14 +13,11 @@ const path= require('path')
 app.use(cors());
 
 
-if(process.env.NODE_ENV === 'production') {
+
     app.use(express.static(path.join(__dirname, './client/build')));
   
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-    });
     
-}
+
 
   
   app.get('/auth', function(req,res){
